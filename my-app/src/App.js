7c2dac4552ccd51de51cc619f484252f1017db43
otherworldly94b <and.charalambous@gmail.com>
2024-03-11 22:26:29 +0000

    add a few challenges

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,18 @@
-import "./App.css";
+import css from "./App.css";
+// import NavBarSimple from "./components/NavBarSimple";
+import Sidebar from "./components/Sidebar";
+import NavBarForm from "./components/NavBarForm";
 import Content from "./components/Content";
 
 function App() {
-  return <div className="App">
+  return (
+    <div className={css.App}>
+      {/* Add your components here */}
+      <NavBarForm />
+      <Sidebar />
       <Content />
-  </div>;
+    </div>
+  );
 }
 
 export default App;
