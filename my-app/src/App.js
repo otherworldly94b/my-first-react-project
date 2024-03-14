32638a399754f8dfea79da9540e5bf907fdd18be
otherworldly94b <and.charalambous@gmail.com>
2024-03-14 19:38:37 +0000

    more react challenges

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,24 @@
-import "./App.css";
-import HTTPHooks from "./components/HTTPHooks";
+import css from "./App.css";
+// import NavBarSimple from "./components/NavBarSimple";
+import Sidebar from "./components/Sidebar";
+import NavBarForm from "./components/NavBarForm";
+//import Content from "./components/Content";
+// import ContentHooks from "./components/ContentHooks";
+// import ContentAPI from "./components/ContentAPI";
+import ContentAPIHooks from "./components/ContentAPIHooks";
 
 function App() {
-  return <div className="App">
-      <HTTPHooks />
-  </div>;
+  return (
+    <div className={css.App}>
+      {/* Add your components here */}
+      <NavBarForm />
+      <Sidebar />
+      {/* <Content /> */}
+      {/* <ContentHooks /> */}
+      {/* <ContentAPI /> */}
+      <ContentAPIHooks />
+    </div>
+  );
 }
 
 export default App;
